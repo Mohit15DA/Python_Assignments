67b4e180c9144a99189ac934d8b130fc2e36a271
Mohit Kumar <mohit2019ec015abesit@gmail.com>
2026-05-06 00:14:54 +0530

    updated

--- UDF_Exercise.py
+++ UDF_Exercise.py
@@ -38,9 +38,24 @@ print(add(a,b))
 
 5. Write a function to find the maximum of three numbers.  
 Program:
+def ismax(a,b,c):
+    if a>=b and a>=c:
+        return a
+    elif b>=a and b>=c:
+        return b
+    else:
+        return c
+print(ismax(5,6,7))
 
 6. Create a function that takes a string and returns it in uppercase.  
+Program:
+def Upper_case(text):
+    return text.upper()
+print(Upper_case("python"))
+        
 7. Write a function to calculate the factorial of a number.  
+Program:
+
 8. Create a function that checks if a number is positive, negative, or zero.  
 9. Write a function to count the number of vowels in a string.  
 10. Create a function that returns the length of a list (without using len()).
@@ -48,9 +63,9 @@ Program:
 
 
 """
+def Upper_case(text):
+    return text.upper()
+print(Upper_case("python"))
+        
 
-
-
-
-
-
+    
