04a780de0fca260232122d5ab6d582ed48d41c69
Mohit Kumar <mohit2019ec015abesit@gmail.com>
2026-06-06 23:38:20 +0530

    Exercise

---
+++ If_Else_Exercise.py
@@ -0,0 +1,27 @@
+"""
+A. Python IF (Single Condition)
+
+1. Write a Python program to check if a number is positive. 
+2. Print "Eligible to vote" if age is 18 or above. 
+3. Check if a number is divisible by 7. 
+4. Print "Pass" if marks are greater than 40. 
+5. Check if a number is greater than 100. 
+6. Display a message if temperature exceeds 45°C. 
+7. Check if a string length is more than 8 characters. 
+8. Print "Logged In" if password matches "admin123". 
+9. Check if a number is a multiple of 10. 
+10. Print a warning if balance is below minimum limit.
+
+B. Python IF–ELSE (Two Conditions)
+
+11. Check whether a number is even or odd. 
+12. Find the largest of two numbers. 
+13. Check whether a person is eligible for driving license. 
+14. Print "Pass" or "Fail" based on marks. 
+15. Check whether a number is positive or negative. 
+16. Check whether a character is a vowel or consonant. 
+17. Check if a year is leap or not. 
+18. Print "Valid Password" or "Invalid Password". 
+19. Determine whether salary is taxable or not. 
+20. Check whether a number is greater than 50 or not.
+"""
