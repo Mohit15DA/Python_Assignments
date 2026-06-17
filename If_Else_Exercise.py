a58a7c69c92f248b3b093010ecb17d037631e234
Mohit Kumar <mohit2019ec015abesit@gmail.com>
2026-06-17 23:00:57 +0530

    Updated

--- If_Else_Exercise.py
+++ If_Else_Exercise.py
@@ -1,19 +1,70 @@
 """
 A. Python IF (Single Condition)
 
-1. Write a Python program to check if a number is positive. 
-2. Print "Eligible to vote" if age is 18 or above. 
-3. Check if a number is divisible by 7. 
-4. Print "Pass" if marks are greater than 40. 
-5. Check if a number is greater than 100. 
-6. Display a message if temperature exceeds 45°C. 
-7. Check if a string length is more than 8 characters. 
-8. Print "Logged In" if password matches "admin123". 
+1. Write a Python program to check if a number is positive.
+Program:
+num= int(input("enter no to check"))
+if num>0:
+ print("Positive")
+
+2. Print "Eligible to vote" if age is 18 or above.
+Program:
+age= int(input("Enter your age :"))
+if age>=18:99
+
+ print("Eligible to vote")
+
+3. Check if a number is divisible by 7.
+Program:
+num= int(input("Enter your number :"))
+if num%7==0:
+ print("Number is Divisible by 7")
+
+4. Print "Pass" if marks are greater than 40.
+Program:
+marks= int(input("Enter your marks :"))
+if marks>40:
+ print("Pass")
+ 
+5. Check if a number is greater than 100.
+Program:
+num= int(input("Enter your number :"))
+if num>100:
+ print(f"{num} is Greater than 100 ")
+ 
+6. Display a message if temperature exceeds 45°C.
+Program:
+temp= int(input("Enter temperature :"))
+if temp>45:
+ print(f"{temp} temperature exceeds ")
+
+7. Check if a string length is more than 8 characters.
+Program:
+st= input("Enter your string :")
+if len(st)>8:
+ print("string has more than 8 characters")
+
+8. Print "Logged In" if password matches "admin123".
+Program:
+password="admin123"
+login_password = input("Enter your password:")
+if login_password== password:
+ print("Logged In")
+ 
 9. Check if a number is a multiple of 10. 
+Program:
+num= int(input("Enter your number:"))
+if num%10==0:
+ print("Multiple of 10")
+
 10. Print a warning if balance is below minimum limit.
+Program:
+bal= int(input("Enter your balance:"))
+minimum_limit=1000
+if bal<minimum_limit :
+ print("Warning :Your balance is low")
 
 B. Python IF–ELSE (Two Conditions)
-
 11. Check whether a number is even or odd. 
 12. Find the largest of two numbers. 
 13. Check whether a person is eligible for driving license. 
